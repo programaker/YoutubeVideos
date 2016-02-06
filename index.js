f7e1ae98c16eb9c788721780bd1e9b22a170169f
programaker <programaker@gmail.com>
2016-02-06 18:51:39 -0200

    Some refactorings and design decision comments

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-(function indexPageModule(global, $) {
+(function indexPageModule($, YoutubeVideos) {
     'use strict';
-    
-    function renderVideoFn($videoLink, $videoThumb) {
-        return function renderVideo(latestVideo) {
-            var fancyboxConfig = {
-                closeBtn: false,
-                padding: 0,
-                maxWidth: 800,
-                maxHeight: 600,
-                fitToView: false,
-                width: '70%',
-                height: '70%',
-                autoSize: false,
-                closeClick: false,
-                openEffect: 'none',
-                closeEffect: 'none'
-            };
 
-            $videoLink
+    //"Porta dos Fundos" channel
+    YoutubeVideos.fetchLatestFromChannel('UCEWHPFNilsT0IfQfutVzsag', renderVideoFn($('#video-link'), $('#video-thumb'))); 
+
+    function renderVideoFn(videoLink, videoThumb) {
+        return function renderVideo(latestVideo) {
+            videoLink
                 .attr('href', latestVideo.videoUrl)
                 .addClass('fancybox.iframe')
-                .fancybox(fancyboxConfig);
+                .fancybox({
+                    closeBtn: false,
+                    padding: 0,
+                    maxWidth: 800,
+                    maxHeight: 600,
+                    fitToView: false,
+                    width: '70%',
+                    height: '70%',
+                    autoSize: false,
+                    closeClick: false,
+                    openEffect: 'none',
+                    closeEffect: 'none'
+                });
             
-            $videoThumb.attr('src', latestVideo.videoThumbnailUrl);
+            videoThumb.attr('src', latestVideo.videoThumbnailUrl);
         };
     }
 
-    var videoLink = $('#video-link');
-    var videoThumb = $('#video-thumb');
-    
-    global.YoutubeVideos.fetchLatestFromChannel(
-        'UCEWHPFNilsT0IfQfutVzsag', //porta dos fundos
-        renderVideoFn(videoLink, videoThumb)); 
-}(window, jQuery));
+}(window.jQuery, window.YoutubeVideos));
--- youtube-videos.js
+++ youtube-videos.js
@@ -1,22 +1,24 @@
-(function youtubeVideosModule(global, $) {
+//closure that make possible: 
+//-> private functions|state|objects|etc
+//-> local 'use strict' directive
+//-> turning module dependencies explicit
+//
+//a negative point: it's a function with side effects (changes window state)
+//but, since it's just a self-executing function that will not be available
+//after module setup, it seems ok... it works like a 'scope', not a true function 
+(function youtubeVideosModule($) {
     'use strict';
 
-    function videoUrl(videoId) {
-        return 'http://www.youtube.com/embed/'+ videoId +'?autoplay=1';
-    }
+    //high-level module definition first 
+    //it's the module's public interface (Revealing Module pattern)
+    //available globally through 'window'
+    
+    window.YoutubeVideos = {
+        fetchLatestFromChannel: fetchLatestVideoFromChannel
+    };    
 
-    function channelVideoSearchUrl(channelId, maxResults) {
-        var url = 'https://www.googleapis.com/youtube/v3/search' +
-            '?key=AIzaSyDQOzdypbd04-ExD90xUVPoEG2Hfx7X3X8' +
-            '&type=video' +
-            '&order=date' +
-            '&part=snippet' +
-            '&channelId=' + channelId +
-            '&maxResults=' + maxResults 
-        ; 
 
-        return url;
-    }
+    //private implementation details after
 
     function fetchLatestVideoFromChannel(channelId, fn) {
         $.ajax({
@@ -25,6 +27,16 @@
             jsonp: 'callback',
             success: fetchVideoSuccessFn(fn)
         });
+    }
+
+    function channelVideoSearchUrl(channelId, maxResults) {
+        return 'https://www.googleapis.com/youtube/v3/search' +
+            '?key=AIzaSyDQOzdypbd04-ExD90xUVPoEG2Hfx7X3X8' +
+            '&type=video' +
+            '&order=date' +
+            '&part=snippet' +
+            '&channelId=' + channelId +
+            '&maxResults=' + maxResults; 
     }
 
     function fetchVideoSuccessFn(fn) {
@@ -40,7 +52,10 @@
         };
     }
 
-    global.YoutubeVideos = {
-        fetchLatestFromChannel: fetchLatestVideoFromChannel
-    };
-}(window, jQuery));
+    function videoUrl(videoId) {
+        return 'http://www.youtube.com/embed/'+ videoId +'?autoplay=1';
+    }
+
+//explicit module dependencies
+//prefixing with 'window.' just to avoid annoying JsHint warnings
+}(window.jQuery));
